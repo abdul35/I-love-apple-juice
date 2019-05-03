50ac13f04ddc00037a1305419ccee7edd8fb704c
abdul35 <abdul355428@bk.ru>
2019-05-03 11:25:29 +0300

    9_8_practical_Py

---
+++ 9_practical_work_Python/pr_work_9.py
@@ -0,0 +1,10 @@
+string = 'STSRAQWE'
+i = string.count('LoL')
+if i == 1:
+    find = string.index('LoL')
+    print(find)
+elif i > 1:    
+    two = string.find('LoL'),string.rfind('LoL')
+    print(two)
+else:
+    print('This String not LoL')
