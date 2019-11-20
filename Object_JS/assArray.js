1a63ce192e0a7e17103f98377db2a53d9cca5171
abdul35 <46883525+abdul35@users.noreply.github.com>
2019-11-20 22:45:39 +0300

    Update assArray.js

--- Object_JS/assArray.js
+++ Object_JS/assArray.js
@@ -2,27 +2,21 @@ let product = [];
 
 function f(n_product, price, discount, n_customer, count_prc) {
     let sum = count_prc * price * discount;
-    
-    
-    
-    product.push({"user_name":n_customer});
-    product.push({"product_name":n_product});
-    product.push({"count_product":count_prc}); 
-    product.push({"dicount":discount});
-    product.push({"sum":sum}); 
-        
-        
-
-
-    return `    Уважаемый - ${product[0]["user_name"]}   
+       
+    product.push({"user_name":n_customer},
+                {"product_name":n_product},
+                {"count_product":count_prc}, 
+                {"dicount":discount},
+                {"sum":sum}); 
+ 
+    let str = `    Уважаемый - ${product[0]["user_name"]}   
     Вы заказали -  ${product[1]["product_name"]}
     в количестве - ${product[2]["count_product"]}
     со скидкой -   ${product[3]["dicount"]}
         
     c Вас:  ${product[4]["sum"]} $ `;
-     
+     return str;
 
 }
 console.log(f("cake", 10, 15, "Abdula", 2));
-
 
